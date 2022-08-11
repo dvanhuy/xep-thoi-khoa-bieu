00eab12b19186bef33ff9eded20fd65b3ca0abb4
unknown <huydinhvan13@gmail.com>
2022-08-11 22:28:00 +0700

    fix

--- Trang3/js3.js
+++ Trang3/js3.js
@@ -18,10 +18,16 @@ btnextstep1.onclick = function(){
     contentstep2.style.transition= '0.5s linear'
     contentstep3.style.transition= '0.5s linear'
     contentstep4.style.transition= '0.5s linear'
+    addoptiontoselect(document.getElementById('step-2-content-select'))
 }
 btnextstep2.onclick = function(){
     contentstep2.classList.add('step-2-hide')
     contentstep3.classList.add('step-3-show')
+    keyclass = document.getElementById('step-2-content-select').value
+    console.log(keyclass);
+    addoptiontoselect(document.getElementById('step-3-content-day'))
+    addoptiontoselect(document.getElementById('step-3-content-start'))
+    addoptiontoselect(document.getElementById('step-3-content-end'))
 }
 btnextstep3.onclick = function(){
     contentstep3.classList.add('step-3-hide')
@@ -41,4 +47,31 @@ btprestep3.onclick = function(){
 btprestep4.onclick = function(){
     contentstep3.classList.remove('step-3-hide')
     contentstep4.classList.remove('step-4-show')
+}
+let filejsonclass,keyclass,dayclass,lessonstart,lessonend
+
+const fileupload = document.getElementById('file-json')
+
+fileupload.addEventListener('change',ev=>{
+    const fileclass= ev.target.files[0]
+    if (fileclass) {
+        var reader = new FileReader();
+        reader.readAsText(fileclass)
+        reader.onload = function(event) {
+            filejsonclass = JSON.parse(event.target.result);
+        };
+    }
+    setTimeout(() => {
+        console.log(filejsonclass);
+    }, 10);
+})
+
+function addoptiontoselect(elementselect){
+    let keyoption ='' 
+    if (filejsonclass) {
+        Object.keys(filejsonclass[0]).forEach(element => {
+            keyoption += '<option value="'+element+'">'+element+'</option>'
+        });
+    }
+    elementselect.innerHTML += keyoption
 }
